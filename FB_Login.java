c69dbe2b7f4a3c314fbfdf9ab5310c4615e155ef
Swati Singh <swati.singh@live.com>
2011-05-30 15:02:38 +0300

    Updated FB_Login

--- FB_Login.java
+++ FB_Login.java
@@ -1,35 +1,15 @@
-package com.example.tests;
-
-import com.thoughtworks.selenium.*;
-import java.util.regex.Pattern;
-
-public class FB_Login extends SeleneseTestCase {
-	public void setUp() throws Exception {
-		setUp("http://www.facebook.com/", "*chrome");
-	}
-	public void testFB_Login() throws Exception {
-package com.example.tests;
-
-import com.thoughtworks.selenium.*;
-import java.util.regex.Pattern;
-
-public class FB_Logout extends SeleneseTestCase {
-	public void setUp() throws Exception {
-		setUp("http://www.facebook.com/", "*chrome");
-	}
-	public void testFB_Logout() throws Exception {
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head profile="http://selenium-ide.openqa.org/profiles/test-case">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link rel="selenium.base" href="http://www.facebook.com/" />
-<title>FB_Logout</title>
+<title>FB</title>
 </head>
 <body>
 <table cellpadding="1" cellspacing="1" border="1">
 <thead>
-<tr><td rowspan="1" colspan="3">FB_Logout</td></tr>
+<tr><td rowspan="1" colspan="3">FB</td></tr>
 </thead><tbody>
 <tr>
 	<td>open</td>
@@ -57,24 +37,10 @@ public class FB_Logout extends SeleneseTestCase {
 	<td></td>
 </tr>
 <tr>
-	<td>click</td>
-	<td>//a[@id='navAccountLink']/img</td>
-	<td></td>
-</tr>
-<tr>
-	<td>clickAndWait</td>
-	<td>//input[@value='Log Out']</td>
-	<td></td>
-</tr>
-<tr>
-	<td>clickAndWait</td>
-	<td>//label/input</td>
+	<td>verifyTextPresent</td>
+	<td>Welcome</td>
 	<td></td>
 </tr>
 </tbody></table>
 </body>
 </html>
-	}
-}
-	}
-}
